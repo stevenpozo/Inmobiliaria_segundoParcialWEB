2f4dfa1d471920c8275af8e31409683755928069
Steven Jefferson Pozo Analuisa <sjpozo1@espe.edu.ec>
2023-07-16 20:31:07 -0500

    actualizado

---
+++ 5. Proyecto/php/registro.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+
+    // Lee el archivo de usuarios existente
+    $users = file("usuarios.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    // Verifica si el correo, usuario o contraseña ya están registrados
+    foreach ($users as $user) {
+        list($existingUsername, $existingPassword, $existingEmail) = explode(':', $user);
+        if ($username === $existingUsername) {
+            die("El nombre de usuario '$username' ya ha sido registrado. Intente con otro.");
+        }
+        if ($email === $existingEmail) {
+            die("El correo electrónico '$email' ya ha sido registrado. Intente con otro.");
+        }
+    }
+
+    // Abre el archivo en modo escritura y crea uno nuevo si no existe
+    $file = fopen("usuarios.txt", "a");
+
+    // Escribe los datos en el archivo
+    fwrite($file, $username . ":" . $password . ":" . $email . "\n");
+
+    // Cierra el archivo
+    fclose($file);
+
+    echo "Registro exitoso. <a href='../html/Login.html'>Iniciar sesión</a>";
+}
+?>
