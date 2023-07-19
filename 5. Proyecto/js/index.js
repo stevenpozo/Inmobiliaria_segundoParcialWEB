71587f3801cca1a1ac391549a995680e393c8a15
Steven Jefferson Pozo Analuisa <sjpozo1@espe.edu.ec>
2023-07-19 00:34:45 -0500

    ya

--- 5. Proyecto/js/index.js
+++ 5. Proyecto/js/index.js
@@ -18,3 +18,104 @@
   });
 });
 
+
+/*cargar htmls*/
+
+function cargarCasas() {
+  // Utilizamos fetch para obtener el contenido de inicio.html
+  fetch('html/Casas.html')
+      .then(response => response.text())
+      .then(data => {
+          // Insertamos el contenido en el div del index.html
+          document.getElementById('index').innerHTML = data;
+
+          // Ocultamos el div "index"
+          //document.getElementById('index').classList.add('hidden');
+      })
+      .catch(error => console.log(error));
+}
+
+function cargarDepa() {
+  // Utilizamos fetch para obtener el contenido de inicio.html
+  fetch('html/Departamentos.html')
+      .then(response => response.text())
+      .then(data => {
+          // Insertamos el contenido en el div del index.html
+          document.getElementById('index').innerHTML = data;
+
+          // Ocultamos el div "index"
+          //document.getElementById('index').classList.add('hidden');
+      })
+      .catch(error => console.log(error));
+}
+
+function cargarTerreno() {
+  // Utilizamos fetch para obtener el contenido de inicio.html
+  fetch('html/terrenos.html')
+      .then(response => response.text())
+      .then(data => {
+          // Insertamos el contenido en el div del index.html
+          document.getElementById('index').innerHTML = data;
+
+          // Ocultamos el div "index"
+          //document.getElementById('index').classList.add('hidden');
+      })
+      .catch(error => console.log(error));
+}
+
+function cargarContactos() {
+  // Utilizamos fetch para obtener el contenido de inicio.html
+  fetch('html/Contactos.html')
+      .then(response => response.text())
+      .then(data => {
+          // Insertamos el contenido en el div del index.html
+          document.getElementById('index').innerHTML = data;
+
+          // Ocultamos el div "index"
+          //document.getElementById('index').classList.add('hidden');
+      })
+      .catch(error => console.log(error));
+}
+
+function cargarInicio() {
+  // Utilizamos fetch para obtener el contenido de inicio.html
+  fetch('html/Inicio.html')
+      .then(response => response.text())
+      .then(data => {
+          // Insertamos el contenido en el div del index.html
+          document.getElementById('index').innerHTML = data;
+
+          // Ocultamos el div "index"
+          //document.getElementById('index').classList.add('hidden');
+      })
+      .catch(error => console.log(error));
+}
+
+function cargarAlquiler() {
+  // Utilizamos fetch para obtener el contenido de inicio.html
+  fetch('html/depa_alquiler.html')
+      .then(response => response.text())
+      .then(data => {
+          // Insertamos el contenido en el div del index.html
+          document.getElementById('index').innerHTML = data;
+
+          // Ocultamos el div "index"
+          //document.getElementById('index').classList.add('hidden');
+      })
+      .catch(error => console.log(error));
+}
+
+function cargarSomos() {
+  // Utilizamos fetch para obtener el contenido de inicio.html
+  fetch('html/somos.html')
+      .then(response => response.text())
+      .then(data => {
+          // Insertamos el contenido en el div del index.html
+          document.getElementById('index').innerHTML = data;
+
+          // Ocultamos el div "index"
+          //document.getElementById('index').classList.add('hidden');
+      })
+      .catch(error => console.log(error));
+}
+
